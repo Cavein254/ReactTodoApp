2eb7fe726707fc20161529a821403f59febb0655
johnmuthua <caveomuthama@gmail.com>
2020-03-24 12:41:27 +0300

    completed edit user form

---
+++ src/components/AddUserForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const AddUserForm = props => {
+  const initialFormState = { id: null, name: "", username: "" };
+  const [user, setUser] = useState(initialFormState);
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+    if (!user.name || !user.username) return;
+    props.addUser(user);
+    setUser(initialFormState);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={handleInputChange}
+        />
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={user.username}
+          onChange={handleInputChange}
+        />
+        <button>Add new User</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUserForm;
